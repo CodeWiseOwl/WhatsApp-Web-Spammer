a126709cb132f93a55ba6b795477b4037ed1af4c
CodeWiseOwl <151384816+CodeWiseOwl@users.noreply.github.com>
2023-11-24 21:46:10 +0530

    Spammer.py

---
+++ Spammer.py
@@ -0,0 +1,14 @@
+import pyautogui
+import webbrowser as wb
+import time
+wb.open("web.whatsapp.com")
+time.sleep(10)
+#you can change the number for i in range to any number you want
+for i in range(1000)
+pyautogui.press("h")
+pyautogui.press("i")
+pyautogui.press(" ")
+pyautogui.press("b")
+pyautogui.press("r")
+pyautogui.press("o")
+pyautogui.press("ENTER")
